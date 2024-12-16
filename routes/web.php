09ed0c04a82eb4008693904f1c917608b4e8aba0
ckyarr <dckygmbl.0@gmail.com>
2024-12-16 16:15:40 +0700

    nambah blade breeze buat role login

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('login');
-});
-Route::get('/register', function () {
-    return view('register');
-});
-Route::get('/dasboard', function () {
-    return view('dasboard');
-});
-Route::get('/classes', function () {
-    return view('classes');
-});
-Route::get('/store', function () {
-    return view('store');
+    return view('welcome');
 });
 
-Route::get('/createaccount', function () {
-    return view('account');
-});
-Route::get('/editaccount', function () {
-    return view('account2');
-});
-Route::get('/createclasses', function () {
-    return view('classes1');
-});
-Route::get('/editclasses', function () {
-    return view('classes2');
-});
-Route::get('/createitem', function () {
-    return view('item');
-});
-Route::get('/edititem', function () {
-    return view('item1');
-});
-Route::get('/adminaccount', function () {
-    return view('dasboardadmin1');
-});
-Route::get('/adminclasses', function () {
-    return view('dasboardadmin2');
-});
-Route::get('/adminitem', function () {
-    return view('dasboardadmin3');
-});
-Route::get('/dasboarduser', function () {
-    return view('dasboardmember');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
+require __DIR__.'/auth.php';
