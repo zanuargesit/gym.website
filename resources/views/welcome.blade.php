f8e23b6b0ac9fbea6d97b40f4ab419b8e8d2e5df
ckyarr <dckygmbl.0@gmail.com>
2024-12-16 18:08:29 +0700

    setting profile fix

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+</head>
+
+<body>
+    <h1>Welcome to Laravel</h1>
+    <a href="{{ route('register') }}">Go to Register</a>
+</body>
+
+</html>
