e94c43f9b4e8e2fcd0bbc9653f8272e5a5f031f2
ZacheusA <manggalayudaalharits@gmail.com>
2025-01-07 08:40:46 +0700

    mengubah tampilan user

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{config('app.name')}}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset ("css/prof.css")}}?v=1">
     @yield('style')
 </head>
 
